1eadb2d730ead33823e02c71de0cb6bfcb311366
Pierre-Alexis Bret <pbret@bess-f2r2s12.clusters.42paris.fr>
2025-02-16 18:39:24 +0100

    * probleme lors de la creatoin du double tab env (invalide read)

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: pbret <pbret@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:25:32 by pbret             #+#    #+#             */
-/*   Updated: 2025/02/14 17:34:16 by pbret            ###   ########.fr       */
+/*   Updated: 2025/02/16 17:49:11 by pbret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,11 @@ int	main(int ac, char **av, char **env)
 		mshell = malloc(sizeof(t_mshell));
 		if (!mshell)
 			ft_error_exit("Error main ");
-		ft_init_mshell(&mshell, env); // initialisation de toutes les struct
-		ft_loop_mshell(&mshell);
+		ft_init_mshell(mshell, env); // initialisation de toutes les struct
+		//ft_loop_mshell(&mshell);
+		ft_free_manag(mshell);
 		free(mshell);
-		rl_clear_history();
+		//rl_clear_history();
 	}
 	else
 	{
--- srcs/utils/init.c
+++ srcs/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: pbret <pbret@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 16:24:50 by pbret             #+#    #+#             */
-/*   Updated: 2025/02/14 17:17:38 by pbret            ###   ########.fr       */
+/*   Updated: 2025/02/16 18:38:51 by pbret            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,38 @@ void	ft_init_mshell(t_mshell *mshell, char **env)
 {
 	mshell->input = NULL;
 	mshell->cmds_list = NULL;
-	mshell->env = env;
-	ft_build_paths(mshell);
+	// mshell->env = env;
+	ft_build_env(mshell, env);
+
+	//mshell->paths = NULL; // aSupp
+	ft_build_paths(mshell); // init. le double tab path
 }
+void	ft_build_env(t_mshell *mshell, char  **env)
+{	
+	int	i;
+	int	counter_line;
+	if (!env)
+		mshell->env = NULL;
+	i = -1;
+	counter_line = 0;
+	while (env[++i])
+		counter_line++;
+	//printf("counter = [%d]\n", counter_line);
+	mshell->env = malloc((counter_line + 1) * sizeof(char *)); //// <<<<<<<<<< +1 ou pas????????????
+	i = -1;
+	while (env[++i])
+	{
+		mshell->env[i] = ft_strdup(env[i]);
+		if (mshell->env[i] == NULL)
+			return ;
+	}
+	env[i] = NULL;
+	i = -1;
+	while (mshell->env[++i])
+		printf("%s\n", mshell->env[i]);
+	//printf("value i : [%d]\n", i);
+}
+
 
 void	ft_build_paths(t_mshell *mshell)
 {
@@ -27,6 +56,11 @@ void	ft_build_paths(t_mshell *mshell)
 	int		j;
 
 	i = -1;
+	if (mshell->env == NULL)
+	{
+		mshell->paths = NULL;
+		return ;
+	}
 	while (mshell->env[++i])
 	{
 		if (!ft_strncmp(mshell->env[i], "PATH=", 5))
@@ -36,7 +70,7 @@ void	ft_build_paths(t_mshell *mshell)
 				j++;
 			paths_line = ft_substr(mshell->env[i], j + 1, ft_strlen(mshell->env[i]));
 			if (!paths_line)
-				mshell->paths == NULL;
+				mshell->paths = NULL;
 			else
 			{
 				mshell->paths = ft_split(paths_line, ':');
