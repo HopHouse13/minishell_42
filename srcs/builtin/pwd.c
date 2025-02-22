4b3d346d542847934d3aae124ef8a8575c94cc5a
Mimir <>
2025-02-22 18:02:23 +0100

    modif builtin pwd, split en 'cwd' pour stockage wd & 'pwd' pour print. non fonctionnel. test GIT

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -1,15 +1,41 @@
 #include "minishell.h"
 
-void	B_pwd(void)
+
+void    BI_env_init(void)
+{
+    char    cwd;
+
+    cwd = malloc(char * 4096);
+    printf("CWD = %s\n", BI_init_cwd(cwd));
+
+}
+
+
+
+char	*BI_init_pwd(char *buff)
 {
 	//printf() + getcwd()	
 	//PATH_MAX = constante dans limits.h
 	//4096 = Linux ; 1024 = MacOs
 
-	char pwd[4096];
+    if (getcwd(buff,sizeof(buff)== NULL))
+    {
+        printf("sizeof cwd : %lu\nlen cwd : %lu\n", sizeof(buff),strlen(buff));
+        perror("PWD = NULL\n");
+        return (NULL);
+    }
+    else
+        return (buff);
 
+	//char pwd[4096];
+
+/*
 	if (getcwd(pwd, sizeof(pwd)) != NULL)
 		printf("PWD = %s\n", pwd);
 	else
 		perror("B_pwd failed !");
+
+
+    */
 }
+
