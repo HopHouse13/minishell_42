b755698b52ac76c67a1190efa62e865f41ccd7b8
Mimir <>
2025-03-11 12:11:39 +0100

    recommencement X / Y...bref, juste commentaire en pseudo code ; rebase d'implementation redirection

---
+++ srcs/Exec/redirect.c
@@ -0,0 +1,21 @@
+#include "minishell.h"
+
+
+
+void    redirect(src((fd)), dest((sens?)))
+{
+    /*
+        Open un fd (open);
+        change les fd de rediction I/O (dup2) ; 2 cas :
+            - Redirecition Input
+            - Redirection Output
+        referme proprement(close)
+     */
+
+
+    //int fd_open;
+    //open (av_arg, 0_RDONLY); open (av_arg, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    //dup2(fd_open, fd);
+    //close
+}
