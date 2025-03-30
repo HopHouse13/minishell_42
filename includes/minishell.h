08cb150a168b0f2d1277fbcad95714fad9ae8e76
Mimir <>
2025-03-30 14:18:45 +0200

    work in progress

--- includes/minishell.h
+++ includes/minishell.h
@@ -42,25 +42,20 @@
 
 typedef enum e_type
 {
-					WORD,
+					ELEM,
 					PIPE, 		// "|"
-					REDIR_IN,	// "<"
-					REDIR_OUT_Q,	// ">"
+					R_IN,	// "<"
+					R_OUT,	// ">" ; TRUNCATE ?
 					HD,			// "<<"
-					END_HD,		// EOF
 					APPEND,		// ">>"
-					DOLLAR,		// "$"
-					S_QUOTE,	// "'"
-					D_QUOTE,	// """
-					END,		// fin de input
-					UNKNOWN		// inconnu
+					F_IN, 	// fichier d'entree
+					F_OUT,	// ecrasement dans fichier
+					F_APP,	// append mode
+					DELIM_HD,	// delimiter heredoc
+					CMD,		// commande
+					ARG,		// argument commande precedente		// option commande
+					END,		// fin input
 }					t_type;
-
-typedef struct s_lexer
-{
-	int 			i;
-	bool			intoken;
-}					t_lexer;
 
 typedef struct s_token
 {
@@ -70,37 +65,76 @@ typedef struct s_token
 	struct s_token	*next;
 }					t_token;
 
+typedef struct s_lexer
+{
+	char			line[SIZE_LINE];
+	t_token			*list_token;
+	int				i;
+	int				j;
+	int				simple_q;
+	int				double_q;
+	bool			APP_HD;
+}					t_lexer;
+
+typedef struct s_hd
+{
+	char	*delim;
+	char	**buff_doc;
+	bool	expand;
+}			t_hd;
+
+typedef	struct s_redir
+{
+	t_type			token;
+	char			*file;
+	struct s_redir	*prev;
+	struct s_redir	*next;
+}			t_redir;
+
 typedef struct s_cmd
 {
 	char			**cmd;
-	char			*path;
+	t_redir			*redir;
+	t_hd			*hd;
+	int				count_hd;
+	bool			simple_q;
+	bool			double_q;
 	struct s_cmd	*prev;
 	struct s_cmd	*next;
 }					t_cmd;
 
-typedef struct s_elem
-{
-	t_cmd			*cmd;
-	struct s_elem	*prev;
-	struct s_elem	*next;
-}					t_elem;
-
 typedef struct s_parser
 {
-	char			line[SIZE_LINE];
 	int				i;
-	int				j;
-	int				squote;
-	int				dquote;
+	t_token		*list_token;
+	t_cmd		*list_cmd;
+	int			simple_q;
+	int			double_q;
+	char		**env;
+	int			exit_status;
 }					t_parser;
 
+typedef struct s_exec // local
+{
+	int				i;
+//	int				exit_status;
+}					t_exec;
+
+typedef	struct	s_mnode
+{
+	void	*ptr;
+	size_t	size;
+	struct	s_mnode	*next;
+}			t_mnode;
 
 typedef struct s_mshell
 {
 	char			*input;
-	t_cmd			*cmds_list;
+	t_token			*list_token;
+	t_cmd			*list_cmd;
+	int				count_pipe;
 	char			**env;
-	char			**paths;
+	char			**paths; 
 	int				exit_status;
 }					t_mshell;
 
@@ -159,10 +193,15 @@ void	redirect_out_app(src, dest);
 void	redirect_hd(src, dest);
 
 	//execution_cmd.c
-void	execution_cmd();
 
 
+bool	ft_check_hd(t_cmd *list_cmd);
+bool	ft_check_BI(t_cmd *list_cmd);
+char	**ft_check_env(char **envp);
 
+int		forker(t_mshell mshell);
+int		pipex(t_mshell mshell);
+void	execution_cmd(t_mshell mshell);
 
 
 #endif
--- srcs/Exec/execution_cmd.c
+++ srcs/Exec/execution_cmd.c
@@ -1,51 +1,110 @@
 #include "../../includes/minshell.h"
 
-/**
+/** EXEC MINISHELL
     STEP : 
-
     1/ FORK
-    2/ REDIRIGE les fichiers
+    2/ REDIRIGE les fichiers——— (IN,OUT, PIPE)
     3/ TRANSFORME & EXECUTE la cmd
-
-
  */
 
+/* ft_executer
+    - check si heredoc
+    - check si BI
+    - check env
+    - check pipe
 
-void    ft_executer(t_mshell instructions)
+    - - fork & redirection
+    - - piping
+
+    - execution cmd
+
+*/
+
+void    ft_executer(t_mshell mshell)
 {
-    //cehck si BI
-    // redirection si necessaire (cf. redirect.c)
-    forker();
-    redirect(t_mshell instructions);
- 
-    // redirect_in ?
+    ft_check_hd(mshell->list_cmd);
+    ft_check_BI(mshell->list_cmd);
+    ft_check_env(mshell->env);
 
-    // redirect_out ?
+    //fork + redirection ?
+    //check si pipe
+    if (mshell->count_pipe)
+        pipex(mshell); // |
+    else
+        forker(mshell); // no pipe
 
+    redirect(mshell); // dans le parent ou dans l'enfant ?
+    
     // commandes
-    execution_cmd();
-
+    execution_cmd(mshell);
+    printf("fin de l'exec.\n");
 }
 
-void    forker()
+
+
+bool check_BI(t_cmd *list_cmd)
 {
-    pid_t pid = fork();
-    if (pid == 0)  // Processus enfant
+    char    BI_list[];
+    int     i;
+
+    BI_list = {"cd", "echo", "env", "exit", "pwd", "export", "unset"};
+    i = 0;
+
+    while (i < 7)
     {
-        // Redirection d'entrée
-        if (infile != -1)
+        if (ft_strchr(list_cmd->cmd[0], BI_list[i]))
+            return (1);
+        i++;
+    }
+    return (0);
+}
+
+char    *check_env()
+{
+    char *path;
+
+    path = getenv("PATH");
+
+    if (!path)
+    {
+        //path = env_mini (pwd, shell lvl & '_')
+        //mallocking en cas de env -i 
+        // ** TO DO ** (voir strat avec Pab)
+    }
+    return ();
+}
+
+void    redirigeur(t_mshell node)
+{
+    // Redirection d'entrée
+        if (infile != -1) // indicateur de redirection
         {
-            dup2(infile, STDIN_FILENO);
+            dup2(infile, STDIN_FILENO); // NEED fichier & mode 
             close(infile);
         }
         // Redirection de sortie
-        if (outfile != -1)
+        if (element->token == REDIR_OUT_Q || element->token == APPEND) // indicateur de redirection
         {
             dup2(outfile, STDOUT_FILENO);
             close(outfile);
         }
+}
+
+
+void    forker(t_mshell element)
+{
+/*
+    FORK
+    REDIRECTION
+    EXECUTION 
+
+*/
+
+    pid_t pid = fork();
+    if (pid == 0)  // Processus enfant
+    {
         // Exécuter la commande
-        execve(cmd_path, args_args, env);
+        execve(element->paths, args_args, element->env);
         perror("execve"); // En cas d'échec
         exit(EXIT_FAILURE);
     }
@@ -53,9 +112,7 @@ void    forker()
 
 
 
-
-
-
+/*
 void    execution_cmd()
 {
     // transformation de la commande
@@ -64,15 +121,14 @@ void    execution_cmd()
     char *envp[];
     envp = getenv() ?? //char* vs char** ? ; //char *getenv(char *name)
 
-
     execve(cmd_path, cmd_args, envp)
-    /*
+    
     if (execve == -1){ // secu
         perror("Execve child1 ");
         ft_free(cmd_args);
         exit(EXIT_FAILURE);
     }
-    */
-
+    
 
 }
+*/
