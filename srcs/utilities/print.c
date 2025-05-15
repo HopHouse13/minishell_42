40461e7d3bc9deca855e03614fc60fda54fdb835
pab <pierrealexisbret@gmail.com>
2025-05-15 15:19:14 +0200

    * Mise en firme du print final du parcer -> fini

--- srcs/utilities/print.c
+++ srcs/utilities/print.c
@@ -6,7 +6,7 @@
 /*   By: pab <pab@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 19:13:42 by pab               #+#    #+#             */
-/*   Updated: 2025/05/15 12:30:36 by pab              ###   ########.fr       */
+/*   Updated: 2025/05/15 15:18:37 by pab              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,16 @@ void	ft_print_list_token(t_token *head) // A SUPP
 
 }
 
-void	ft_print_list_cmd(t_mshell *mshell) // A SUPP
+void	ft_print_header_list_cmd(void)
+{
+	printf(CYAN"\t╔═════════════════════════════════════════════════╗\n");
+	printf("\t║\t\t\t\t\t\t  ║\n");
+	printf(GREEN"\t\t\t    \033[1m[ LIST_CMD ]\n");
+	printf(MAGENTA"\t║\t\t\t\t\t\t  ║\n");
+	printf("\t╚═════════════════════════════════════════════════╝\n%s", RESET);
+}
+
+void	ft_print_list_cmd(t_mshell *mshell)
 {
 	t_cmd	*tmp;
 	int		counter;
@@ -65,23 +74,19 @@ void	ft_print_list_cmd(t_mshell *mshell) // A SUPP
 	tmp = mshell->list_cmd;
 	if (!tmp)
 		return ;
-	printf(CYAN"\t╔═══════════════════════════════════════════════════╗\n");
-	printf("\t║\t\t\t\t\t\t    ║\n");
-	printf(GREEN"\t\t\t    \033[1m[ LIST_CMD ]\n");
-	printf(MAGENTA"\t║\t\t\t\t\t\t    ║\n");
-	printf("\t╚═══════════════════════════════════════════════════╝\n%s", RESET);
+	ft_print_header_list_cmd();
 	while (tmp != NULL)
 	{
-		printf("\n\t\t\t--- NODE Nº %d ---\n\n", counter++);
+		printf(GREEN"\n\t\t\t\033[1m--- NODE Nº %d ---\n\n", counter++);
 		ft_print_double_tab(tmp->cmd);
-		printf("\t\tBUILTIN?\t->\t[%d]\n", tmp->builtin);
-		printf("\t\tFD_INFILE\t->\t[%d]\n", tmp->fd_in);
-		printf("\t\tFD_OUTFILE\t->\t[%d]\n", tmp->fd_out);
-		printf("\t\tFD_HD\t\t->\t[%d]\n", tmp->fd_hd);
-		printf("\t\tDELIM_HD\t->\t[%s]\n", tmp->delim_hd);
-		printf("\t\tEXPAND_HD?\t->\t[%d]\n%s", tmp->expand_hd, RESET);
+		printf("\t\tBUILTIN?\t->\t[ %d ]\n", tmp->builtin);
+		printf("\t\tFD_INFILE\t->\t[ %d ]\n", tmp->fd_in);
+		printf("\t\tFD_OUTFILE\t->\t[ %d ]\n", tmp->fd_out);
+		printf("\t\tFD_HD\t\t->\t[ %d ]\n", tmp->fd_hd);
+		printf("\t\tDELIM_HD\t->\t[ %s ]\n", tmp->delim_hd);
+		printf("\t\tEXPAND_HD?\t->\t[ %d ]\n%s", tmp->expand_hd, RESET);
 		tmp = tmp->next;
-		printf(CYAN"\n\t═════════════════════════════════════════════════════\n");
-		printf(MAGENTA"\t═════════════════════════════════════════════════════\n%s", RESET);
+		printf(CYAN"\n\t\t══════════════════════════════════\n");
+		printf(MAGENTA"\t\t══════════════════════════════════\n%s", RESET);
 	}
 }
