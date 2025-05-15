c2bdab8b48b18d5b89117d98c451ae9794ec4e7c
pab <pierrealexisbret@gmail.com>
2025-05-15 23:03:07 +0200

    * heredoc, premiere input null -> invalid read   -> fixed * modification du lexer pour ne plus utiliser une string a 80k char dans la stack (1 calloc+ 1 malloc dans la heap)

--- srcs/lexer/lexer_cleaning_input.c
+++ srcs/lexer/lexer_cleaning_input.c
@@ -6,18 +6,46 @@
 /*   By: pab <pab@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 17:31:08 by pbret             #+#    #+#             */
-/*   Updated: 2025/05/12 20:52:09 by pab              ###   ########.fr       */
+/*   Updated: 2025/05/15 22:51:22 by pab              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void	ft_handle_space(t_lexer *lexer, char *wild_input, t_mnode **ml)
+int	ft_count_char(char *line)
+{
+	int		i;
+	int		count;
+	bool	in_space;
+
+	i = 0;
+	count = 0;
+	in_space = false;
+	while (line[i] == ' ')
+		i++;
+	while (line[i])
+	{
+		if (line[i] != ' ')
+		{
+			in_space = false;
+			count++;
+		}
+		else if (line[i] == ' ' && !in_space)
+		{
+			in_space = true;
+			count++;
+		}
+		i++;
+	}
+	return (count);
+}
+
+void	ft_build_clear_input(t_lexer *lexer, char *wild_input, t_mnode **ml)
 {
 	bool	in_space;
 	
-	lexer->input_clear = ft_malloc_list(sizeof(char) *
-							ft_strlen(wild_input) + 1, ml);
+	lexer->clear_input = ft_malloc_list(sizeof(char) *
+							ft_count_char(wild_input) + 1, ml);
 	lexer->i = 0;
 	lexer->j = 0;
 	in_space = false;
@@ -28,16 +56,16 @@ void	ft_handle_space(t_lexer *lexer, char *wild_input, t_mnode **ml)
 		if (wild_input[lexer->i] != ' ')
 		{
 			in_space = false;
-			lexer->input_clear[lexer->j++] = wild_input[lexer->i];
+			lexer->clear_input[lexer->j++] = wild_input[lexer->i];
 		}
 		else if (wild_input[lexer->i] == ' ' && in_space == false)
 		{
 			in_space = true;
-			lexer->input_clear[lexer->j++] = wild_input[lexer->i];
+			lexer->clear_input[lexer->j++] = wild_input[lexer->i];
 		}
 		lexer->i++;
 	}
-	lexer->input_clear[lexer->j] = '\0';
+	lexer->clear_input[lexer->j] = '\0';
 }
 
 void	ft_put_redirection(t_lexer *lexer, char *input)
@@ -60,10 +88,10 @@ void	ft_put_pipe(t_lexer *lexer)
 	lexer->wild_input[++lexer->j] = ' ';
 }
 
-void	ft_input_one_space(t_mshell *mshell, t_lexer *lexer, t_mnode **ml)
+void	ft_cleaning_input(t_mshell *mshell, t_lexer *lexer, t_mnode **ml)
 {printf("\n\n\t--------------------- ONE_SPACE ---------------------\n");
 	lexer->i = 0;
-	while (mshell->input && lexer->i < SIZE_LINE && mshell->input[lexer->i])
+	while (mshell->input && mshell->input[lexer->i])
 	{
 		if (ft_status_qts(mshell->qts, mshell->input, lexer->i)
 			|| ft_effect_esc(mshell->qts, mshell->input, lexer->i))
@@ -82,6 +110,7 @@ void	ft_input_one_space(t_mshell *mshell, t_lexer *lexer, t_mnode **ml)
 	}
 	lexer->wild_input[++lexer->j] = ' ';
 	lexer->wild_input[++lexer->j] = ';';
-	ft_handle_space(lexer, lexer->wild_input, ml);
-	ft_print_input_clean(lexer->input_clear); // ASUPP
+	lexer->wild_input[++lexer->j] = '\0'; // pas oublie car calloc mais je prefere
+	ft_build_clear_input(lexer, lexer->wild_input, ml);
+	ft_print_input_clean(lexer->clear_input); // ASUPP
 }
--- srcs/lexer/lexer_initialisation.c
+++ srcs/lexer/lexer_initialisation.c
@@ -6,16 +6,17 @@
 /*   By: pab <pab@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 17:41:17 by pbret             #+#    #+#             */
-/*   Updated: 2025/05/12 22:23:35 by pab              ###   ########.fr       */
+/*   Updated: 2025/05/15 22:51:22 by pab              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-void ft_init_lexer(t_lexer *lexer)
+void ft_init_lexer(t_mshell *mshell, t_lexer *lexer, t_mnode **ml)
 {
-	ft_init_line(lexer->wild_input);
-	lexer->input_clear = NULL;
+	lexer->wild_input = NULL;
+	ft_init_wild_input(mshell, lexer, ml);
+	lexer->clear_input = NULL;
 	lexer->list_token = NULL;
 	lexer->i = -1;
 	lexer->j = -1;
